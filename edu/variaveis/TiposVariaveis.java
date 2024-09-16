077ef674e268c6d1223d6f4911169b6e11d87b94
dev-davimarques <davimarques10.dm@icloud.com>
2024-09-16 09:56:05 -0300

    Teste de reescrita

--- edu/variaveis/TiposVariaveis.java
+++ edu/variaveis/TiposVariaveis.java
@@ -1,2 +1,8 @@
+package edu.variaveis;
+
 public class TiposVariaveis {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Testando uma nova escrita!");
+    }
 }
